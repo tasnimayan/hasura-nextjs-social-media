de718870850ad3e6c5804113a8fcdf7f850e0844
Tasnim Ayan <tasnimayan22@gmail.com>
2025-09-15 15:39:44 +0600

    feed screen flash before auth issue fixed

--- src/app/(protected)/layout.tsx
+++ src/app/(protected)/layout.tsx
@@ -1,13 +1,17 @@
-import AuthWrapper from "./auth-wrapper";
 import Navbar from "@/components/layout/feed/navbar";
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+import { redirect } from "next/navigation";
 
-export default function ProtectedLayout({ children }: Readonly<{ children: React.ReactNode }>) {
+export default async function ProtectedLayout({ children }: Readonly<{ children: React.ReactNode }>) {
+  const session = await getServerSession(authOptions);
+
+  if (!session) redirect("/login");
+
   return (
     <div className="flex flex-col h-dvh w-full overflow-hidden">
-      <AuthWrapper>
-        <Navbar />
-        <main className="bg-[#EFF6FF]">{children}</main>
-      </AuthWrapper>
+      <Navbar />
+      <main className="bg-[#EFF6FF]">{children}</main>
     </div>
   );
 }
